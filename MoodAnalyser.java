ee1537f7f47c1c6a02b60958fe10e2559a9cc986
amod1811 <ammch101@gmail.com>
2022-05-17 01:50:26 +0530

    Mood Analyse program

--- MoodAnalyser.java
+++ MoodAnalyser.java
@@ -3,9 +3,9 @@ package bl.com.moodanalyser;
 public class MoodAnalyser {
     static String message;
     public String analysemood(String message) {
-        if (message.equalsIgnoreCase("I am in sad mood"))
+        if (message.equalsIgnoreCase("I am in sad Mood"))
             return "SAD";
-        else if (message.equalsIgnoreCase("I am in happy mood"))
+        else if (message.equalsIgnoreCase("I am in happy Mood"))
             return "HAPPY";
         else
           return "NO Match";
