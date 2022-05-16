4bd405408312b782414a4f9622a25617c40bfbc9
amod1811 <ammch101@gmail.com>
2022-05-17 01:04:07 +0530

    Ability to analyse and respond Happy or Sad mood

---
+++ MoodAnalyserMain.java
@@ -0,0 +1,19 @@
+package bl.com.moodanalyser;
+/*
+Ability to analyse and respond Happy or Sad mood
+ */
+public class MoodAnalyserMain {
+    public static void main(String[] args) {
+        System.out.println("Welcome to mood Analyser program");
+
+        //Crate object
+        MoodAnalyser moodAnalyser = new MoodAnalyser();
+
+        // pass Happy message
+        System.out.println(moodAnalyser.analysemood("I am in happy mood"));
+
+        //pass sad message
+
+        System.out.println(moodAnalyser.analysemood("I am in  Sad Mood"));
+    }
+}
