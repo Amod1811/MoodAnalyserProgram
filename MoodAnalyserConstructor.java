5ad98e96f935a41826007082469b19ebf173be30
amod1811 <ammch101@gmail.com>
2022-05-17 17:51:33 +0530

    Using the constuctor modd analyser sad or happy

---
+++ MoodAnalyserConstructor.java
@@ -0,0 +1,27 @@
+package com.bl.moodanalyzerrprogram;
+
+public class MoodAnalyserConstructor {
+	
+	
+	String happy="I am in Happy Mood";
+	String sad = "I am in Sad Mood";
+	
+	public MoodAnalyserConstructor() {
+		
+		this.happy = happy;
+		this.sad = sad;
+	}
+	
+	public MoodAnalyserConstructor(String happy, String sad) {
+		System.out.println("I am in Happy Mood");
+		System.out.println("i am in Sad Mood");
+	}
+	
+	public static void main(String[] args) {
+		MoodAnalyserConstructor obj = new MoodAnalyserConstructor();
+		
+	MoodAnalyserConstructor obj1 = new MoodAnalyserConstructor("sad", "happy");
+
+	}
+
+}
